e9a1a959bf426ae6a57e049f65d03e641ff16cbe
rayseo <rayseo@carrotglobal.com>
2022-05-10 22:37:48 +0900

    프론트 로그인 로직 구현

--- src/views/loginView.vue
+++ src/views/loginView.vue
@@ -8,14 +8,14 @@
                         <v-spacer></v-spacer>
                     </v-toolbar>
                     <v-card-text>
-                        <v-form>
-                            <v-text-field prepend-icon="mdi-account"  label="아이디" type="text"></v-text-field>
-                            <v-text-field prepend-icon="mdi-lock"  label="비밀번호" type="password"></v-text-field>
+                        <v-form v-model="input.valid">
+                            <v-text-field prepend-icon="mdi-account"  label="아이디" type="text" v-model="input.id" :rules="input.idRules" required></v-text-field>
+                            <v-text-field prepend-icon="mdi-lock"  label="비밀번호" type="password" v-model="input.password" :rules="input.passRules" required></v-text-field>
                         </v-form>
                     </v-card-text>
                     <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn color="primary" >로그인</v-btn>
+                        <v-btn color="primary" @click="login()">로그인</v-btn>
                         <v-btn color="primary" @click="goJoin()">회원가입</v-btn>
                     </v-card-actions>
                 </v-card>
@@ -26,7 +26,43 @@
 <script>
     export default {
         name :"loginView",
+        data: ()=>({
+            input:{
+                valid:false,
+                id:"",
+                password:"",
+                idRules:[
+                    v => !!v || '아이디를 입력해주세요.'
+                ],
+                passRules:[
+                    v => !!v || '비밀번호를 입력해주세요.'
+                ]
+            }
+        }),
         methods:{
+
+            /**
+             * @description 로그인
+             */
+            login(){
+                if(this.input.valid){
+                    let params = {
+                        id : this.input.id,
+                        pass : this.input.password
+                    }
+                    this.axios.post("/login",params).then((res)=>{
+                        console.log(res);
+                    }).catch((err)=>{
+                        console.log(err);
+                    });
+                }else{
+                    alert("필수 값을 입력해주세요.");
+                }
+            },
+
+            /**
+             * @description 회원가입 페이지 이동
+             */
             goJoin(){
                 this.$router.push('join')
             }
